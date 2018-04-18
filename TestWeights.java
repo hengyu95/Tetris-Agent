c648a48f8ed29aa8a4437523e3d95016960dd033
Hengyu <limhengyu95@gmail.com>
2018-04-18 21:52:01 +0800

    Updated with write to file

--- TestWeights.java
+++ TestWeights.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
@@ -11,11 +14,25 @@ public class TestWeights {
     private static ExecutorService es;
     private static ArrayList<Callable<Double>> tasks;
     private static int NUM_GAMES_TO_PLAY = 50;
+    public static String WEIGHTS_RESULT_FILE = "TestWeights.out";
 
     // set the weights to test here!
-    private static double[] weights = { -0.66815299 , -0.18275129 , -0.00313774 , -0.61375989 , -0.10106387 , -0.26484914 , -0.09336810 , -0.23320790 };
+    private static double[] weights = {-0.41253368, -0.19708301, -0.00863855, -0.74445169, -0.10845239, -0.38149603, -0.01316676, -0.28145480};
 
-    public static void main (String[] args) throws InterruptedException, ExecutionException {
+    private static void writeToFile(double average) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.currentTimeMillis() + WEIGHTS_RESULT_FILE));
+        bw.write(average + "\n");
+
+        for (double weight : weights) {
+            bw.write(Double.toString(weight) + " ");
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
         NUM_THREADS = Runtime.getRuntime().availableProcessors();
         System.out.println("# processors available = " + NUM_THREADS);
         es = Executors.newFixedThreadPool(NUM_THREADS);
@@ -30,11 +47,12 @@ public class TestWeights {
 
         double average = 0;
         List<Future<Double>> results = es.invokeAll(tasks);
-        for (Future<Double> result: results) {
+        for (Future<Double> result : results) {
             average += result.get();
         }
 
         Debug.printRed("Average score = " + average / NUM_GAMES_TO_PLAY + " over " + NUM_GAMES_TO_PLAY + " games");
+        writeToFile(average);
         System.exit(0);
     }
 }
